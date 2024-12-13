ab38cd062e7ea2562877e9219787888582101afc
mani14dania <mani14@student.eadania.dk>
2024-12-13 19:20:20 +0100

    Små rettelser igen i SQL

--- sql_1eksamen.R
+++ sql_1eksamen.R
@@ -1,8 +1,8 @@
-pacman::p_load(DBI, dbplyr, tidyverse, RSQLite)
+pacman::p_load(DBI, RSQLite)
 
 vff <- readRDS("vff.rds")
 dmi <- readRDS("dmi_færdig.rds")
-superstats <- read.csv("tilskuere.csv")
+superstats <- read.csv("tilskuere.csv") # ret filnavn og format til "superstats.rds" og få det til at hænge sammen med superstats_1eksamen.R
 
 connection <- dbConnect(SQLite(), "vff.db") 
 
@@ -10,12 +10,14 @@ dbWriteTable(connection, "vff", vff)
 dbWriteTable(connection, "superstats", superstats)
 dbWriteTable(connection, "dmi", dmi)
 
-?dbWriteTable
-
 dbListTables(connection)
 
+# Få rettet query når alle tabeller er klar
 samlet_vff <- dbGetQuery(connection,
            "select vff.*, superstats.*, dmi.*
            from guld
            left join superstats ON vff.dato = superstats.dato
            left join dmi ON vff.dato = dmi.dato")
+
+# Eksporterer samlet tabel i rds-fil til videre forarbejdning
+write_rds(samlet_vff, "samlet_vff.rds")
