fe8beb50c99a4a53d1123ae8f0328fc1bf3bf5b7
mani14dania <mani14@student.eadania.dk>
2024-12-19 14:00:37 +0100

    Dataset og ML

--- machine_learning_1eksamen.R
+++ machine_learning_1eksamen.R
@@ -1,8 +1,11 @@
 # Machine Learning ------------------------------------------------------
 pacman::p_load( tidyverse, tidymodels, caret, ISLR2, glmnet, boot, leaps,
-               viridis, pls)
+               viridis, pls, car)
 
-samlet_data_flot <- read_rds("data/samlet_data_flot.rds")
+dataset <- read_rds("data/dataset.rds")
+
+x <- model.matrix(antal_afhentede ~ . - 1, dataset)
+y <- dataset$antal_afhentede
 
 # Udtræk kun de numeriske kolonner
 numeriske_variabler <- samlet_data_flot[, sapply(samlet_data_flot, is.numeric)]  
@@ -18,33 +21,27 @@ corrplot(cor_matrix, method = "color", type = "upper", order = "hclust", tl.col 
 
 pairs(numeriske_variabler)
 
+model <- lm(antal_afhentede ~ . , data = dataset)
+
+vif(model)
+
+summary(model)
+
+cor_matrix <- cor(x)
+cor_long <- as.data.frame(as.table(cor_matrix)) |> 
+  rename(Korrelation = Freq) |> 
+  filter(Var1 != Var2) |> 
+  filter(abs(Korrelation) > 0.75) |> 
+  arrange(desc(Korrelation))
 
 # Ridge og Lasso --------------------------------------------
-
-x <- model.matrix(antal_spild ~ . - 1, samlet_data_flot)
-y <- samlet_data_flot$antal_spild
-
-sum(is.na(samlet_data_flot))
-colSums(is.na(samlet_data_flot))
-
 model <- lm(y ~ x)
-
-# Udtræk R²
-summary(model)$r.squared
-
-length(y)
-length(x)
-
-sum(is.na(y))
-sum(is.na(x_variabler))
-
 grid <- 10^seq(10, -2, length = 100)
 
 set.seed(123)
 train <- sample(1:nrow(x), nrow(x)*2/3)
 test <- (-train)
 y.test <- y[test]
-
 
 # Ridge
 ridge.mod <- glmnet(
@@ -94,9 +91,9 @@ rmse_lasso_test <- sqrt(mean((lasso.pred - y.test)^2))
 
 # 0 features
 set.seed(123)
-glm.fit <- glm(antal_spild ~ 1, data = samlet_data_flot[train, ])
-rmse_0_cv <- sqrt(cv.glm(samlet_data_flot[train, ], glm.fit , K = 10)$delta[1])
-rmse_0_test <- sqrt(mean((samlet_data_flot[test, ]$antal_spild - predict(glm.fit, samlet_data_flot[test, ]))^2))
+glm.fit <- glm(antal_afhentede ~ 1, data = dataset[train, ])
+rmse_0_cv <- sqrt(cv.glm(dataset[train, ], glm.fit , K = 10)$delta[1])
+rmse_0_test <- sqrt(mean((dataset[test, ]$antal_afhentede - predict(glm.fit, dataset[test, ]))^2))
 
 
 # Best subset selection
@@ -109,8 +106,8 @@ predict.regsubsets <- function(object, newdata, id, ...) {
   mat[, xvars] %*% coefi
 }
 
-samlet_train <- samlet_data_flot[train,]
-samlet_test <- samlet_data_flot[test,]
+samlet_train <- dataset[train,]
+samlet_test <- dataset[test,]
 
 k <- 10 # Vi danner 10 folds
 n <- nrow(samlet_train) # registrerer hvor mange observationer, vi har.
@@ -118,20 +115,20 @@ set.seed(1)
 folds <- sample(rep(1:k, length = n)) #Vi tildeler en værdi mellem 1 og
 dim(samlet_train)[2]  # Der er 14 variabler og dermed 13 prædiktorer
 
-cv.errors <- matrix(NA, k, 19,
+cv.errors <- matrix(NA, k, 14,
                     dimnames = list(NULL, paste(1:19)))
 cv.errors
 
 
 for (j in 1:k) { # her gennemløbes alle folds
-  best.fit <- regsubsets(antal_spild ~ .,
+  best.fit <- regsubsets(antal_afhentede ~ .,
                          data = samlet_train[folds != j, ],
                          nvmax = 13)
   for (i in 1:19) { # her gennemløbes alle kandidatmodeller
     pred <- predict(best.fit, samlet_train[folds == j, ], id = i)
     # predict-funktionen ovenfor kalder den funktion, vi har lavet tidligere. 
     cv.errors[j, i] <-
-      mean((samlet_train$antal_spild[folds == j] - pred)^2) # Her udregnes MSE for hver 
+      mean((samlet_train$antal_afhentede[folds == j] - pred)^2) # Her udregnes MSE for hver 
     # fold for hver kandidatmodel 
   }
 }
@@ -146,14 +143,14 @@ which.min(mean.cv.errors)
 
 
 # Her fittes modellen til ALLE træningsdata
-reg.best <- regsubsets(antal_spild ~ ., data = samlet_train,
+reg.best <- regsubsets(antal_afhentede ~ ., data = samlet_train,
                        nvmax = 13)
 coef(reg.best, 7)
 
 pred_best_subset <- predict(reg.best, samlet_test, id = 7)
 
 
-mse_best_subset <- mean((samlet_data_flot[test,]$antal_spild - pred_best_subset)^2)
+mse_best_subset <- mean((dataset[test,]$antal_afhentede - pred_best_subset)^2)
 rmse_bestsubset_test <- sqrt(mse_best_subset)
 rmse_bestsubset_cv <- sqrt(min(mean.cv.errors))
 
